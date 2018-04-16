21b36298af555a29b4bd811115cfb61734f4089b
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-04-16 09:08:33 +0200

    Add project.clj #1

---
+++ project.clj
@@ -0,0 +1,16 @@
+(defproject clj-comgate "0.0.0"
+  :description "A client for Comgate Payments API"
+  :url "https://github.com/druids/clj-comgate"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+
+  :dependencies [[http-kit "2.2.0"]]
+
+  :profiles {:dev {:plugins [[lein-cloverage "1.0.10"]
+                             [lein-kibit "0.1.6"]
+                             [jonase/eastwood "0.2.5"]
+                             [venantius/ultra "0.5.2"]]
+                   :dependencies [[org.clojure/clojure "1.9.0"]]
+                   :source-paths ["src" "dev/src"]}
+             :test {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :aliases {"coverage" ["with-profile" "test" "cloverage" "--fail-threshold" "95" "-e" "dev|user"]})
